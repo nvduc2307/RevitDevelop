6d586ff9aebfeebdd1944b26e9c3a46b3c281704
nvduc2307 <nvduc.yt@gmail.com>
2024-11-18 17:56:49 +0700

    update

---
+++ Tools/Rebars/InstallRebarBeamV2/iservices/IDrawRebarBeamInCanvas.cs
@@ -0,0 +1,9 @@
+﻿using RevitDevelop.Tools.Rebars.InstallRebarBeamV2.models;
+
+namespace RevitDevelop.Tools.Rebars.InstallRebarBeamV2.iservices
+{
+    public interface IDrawRebarBeamInCanvas
+    {
+        public void DrawSectionBeamConcrete(RebarBeam rebarBeam);
+    }
+}
--- Tools/Rebars/InstallRebarBeamV2/viewModels/InstallRebarBeamV2ViewModel.cs
+++ Tools/Rebars/InstallRebarBeamV2/viewModels/InstallRebarBeamV2ViewModel.cs
@@ -1,5 +1,8 @@
-﻿using RevitDevelop.Tools.Rebars.InstallRebarBeamV2.models;
+﻿using RevitDevelop.Tools.Rebars.InstallRebarBeamV2.iservices;
+using RevitDevelop.Tools.Rebars.InstallRebarBeamV2.models;
 using RevitDevelop.Tools.Rebars.InstallRebarBeamV2.views;
+using System.Windows.Controls;
+using Utils.canvass;
 
 namespace RevitDevelop.Tools.Rebars.InstallRebarBeamV2.viewModels
 {
@@ -7,10 +10,29 @@ namespace RevitDevelop.Tools.Rebars.InstallRebarBeamV2.viewModels
     {
         public ElementInstances ElementInstances { get; set; }
         public InstallRebarBeamView MainView { get; set; }
-        public InstallRebarBeamV2ViewModel()
+        public CanvasPageBase CanvasPageSectionStart { get; set; }
+        public CanvasPageBase CanvasPageSectionMid { get; set; }
+        public CanvasPageBase CanvasPageSectionEnd { get; set; }
+        private IDrawRebarBeamInCanvas _drawRebarBeamInCanvas;
+        public InstallRebarBeamV2ViewModel(IDrawRebarBeamInCanvas drawRebarBeamInCanvas)
         {
             ElementInstances = new ElementInstances();
             MainView = new InstallRebarBeamView() { DataContext = this };
+            MainView.Loaded += MainView_Loaded;
+            InitAction();
+            _drawRebarBeamInCanvas = drawRebarBeamInCanvas;
+        }
+
+        private void MainView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CanvasPageSectionStart = new CanvasPageBase(MainView.FindName("CanvasSectionStart") as Canvas);
+            CanvasPageSectionMid = new CanvasPageBase(MainView.FindName("CanvasSectionMid") as Canvas);
+            CanvasPageSectionEnd = new CanvasPageBase(MainView.FindName("CanvasSectionEnd") as Canvas);
+            _drawRebarBeamInCanvas.DrawSectionBeamConcrete(ElementInstances.RebarBeamActive);
+        }
+        private void InitAction()
+        {
+
         }
     }
 }
